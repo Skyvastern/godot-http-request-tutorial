b8492bf8e569ba61dbf3dcc2c549fea0b4289744
Arpit Srivastava <skyvastern@gmail.com>
2024-01-20 12:38:48 +0530

    Create global singleton

---
+++ frontend/main/common/singletons/global/global.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var access_token: String
+
+
+func get_form_data(data: Dictionary) -> String:
+	var parts: Array[String] = []
+	
+	for key in data.keys():
+		var value: String = str(key).uri_encode() + "=" + str(data[key]).uri_encode()
+		parts.append(value)
+	
+	return "&".join(parts)
