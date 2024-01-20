ae73340c264ed423a00bbccd41cfc5f215bec479
Arpit Srivastava <skyvastern@gmail.com>
2024-01-20 13:19:23 +0530

    Update and add home_btn in login/signup menus

--- frontend/main/common/home_btn/home_btn.gd
+++ frontend/main/common/home_btn/home_btn.gd
@@ -2,6 +2,7 @@ extends Button
 class_name HomeBtn
 
 @export_file("*.tscn") var main_menu_path: String
+@export_file("*.tscn") var authentication_menu_path: String
 
 
 func _ready() -> void:
@@ -9,8 +10,15 @@ func _ready() -> void:
 
 
 func _on_pressed() -> void:
-	var main_menu_res: Resource = load(main_menu_path)
-	var main_menu: MainMenu = main_menu_res.instantiate()
-	get_parent().get_parent().add_child(main_menu)
-	
-	get_parent().queue_free()
+	if Global.access_token == "":
+		var authentication_menu_res: Resource = load(authentication_menu_path)
+		var authentication_menu: Authentication = authentication_menu_res.instantiate()
+		get_parent().get_parent().add_child(authentication_menu)
+		
+		get_parent().queue_free()
+	else:
+		var main_menu_res: Resource = load(main_menu_path)
+		var main_menu: MainMenu = main_menu_res.instantiate()
+		get_parent().get_parent().add_child(main_menu)
+		
+		get_parent().queue_free()
