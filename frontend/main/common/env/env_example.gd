f44c8dede1680e22f691f9824edea05897ce5a33
Arpit Srivastava <skyvastern@gmail.com>
2024-01-24 20:32:26 +0530

    Update env instructions

--- frontend/main/common/env/env_example.gd
+++ frontend/main/common/env/env_example.gd
@@ -1,8 +1,8 @@
 # This is an example ENV file, as the ENV file itself is not pushed to GitHub
 # To make use of ENV, do the following -
-# 1. Rename env_example.gd -> env.gd
-# 2. Rename class name ENV_EXAMPLE -> ENV
-# 3. Fill in the variable values with your API keys
+# 1. Duplicate env_example.gd and name the new file to env.gd
+# 2. Rename env.gd's class name from ENV_EXAMPLE -> ENV
+# 3. In env.gd, fill in the variable values with your API keys
 
 extends Node
 class_name ENV_EXAMPLE
